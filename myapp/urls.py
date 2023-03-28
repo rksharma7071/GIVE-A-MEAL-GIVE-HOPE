525c255517beb68cdc4228bf89ed820e9dcc4beb
rksharma7071 <rksharma7071@gmail.com>
2023-03-29 00:30:09 +0530

    commit 1.2

---
+++ myapp/urls.py
@@ -0,0 +1,36 @@
+"""waste_food_management_and_donation URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('why_us/', views.why_us, name='why_us'),
+    path('food_for_all/', views.food_for_all, name='food_for_all'),
+    path('zero_food_wastage/', views.zero_food_wastage, name='zero_food_wastage'),
+    path('community_fridge/', views.community_fridge, name='community_fridge'),
+    path('meals_on_wheele/', views.meals_on_wheele, name='meals_on_wheele'),
+    path('our_program/', views.our_program, name='our_program'),
+    path('our_donators/', views.our_donators, name='our_donators'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('donate/', views.donate, name='donate'),
+    path('loginView/', views.loginView, name='loginView'),
+    path('logoutView/', views.logoutView, name='logoutView'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    
+]
---
+++ myapp/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'home.html')
+
+def why_us(request):
+    return render(request, 'why_us.html')
+
+def food_for_all(request):
+    return render(request, 'food_for_all.html')
+
+def zero_food_wastage(request):
+    return render(request, 'zero_food_wastage.html')
+
+def community_fridge(request):
+    return render(request, 'community_fridge.html')
+
+def meals_on_wheele(request):
+    return render(request, 'meals_on_wheele.html')
+
+def our_program(request):
+    return render(request, 'our_program.html')
+
+def our_donators(request):
+    return render(request, 'our_donators.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def donate(request):
+    return render(request, 'donate.html')
+
+@login_required(redirect_field_name='loginView',login_url='loginView')
+def dashboard(request):
+    return render(request, 'dashboard.html', locals())
+
+def loginView(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user  = authenticate(request, username=username, password=password)
+        if user is None:
+            error = 'yes'
+        else:
+            login(request, user)
+            error = 'no'
+            return redirect('dashboard')
+    return render(request, 'login.html', locals())
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+            first_name = request.POST.get('first_name')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+
+            if password == confirm_password:
+                try:
+                    user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name)
+                    messages.success(request, 'Your account has been created!')
+                    return redirect('dashboard')
+                except Exception as e:
+                    messages.error(request, f'{e}')
+            else:
+                messages.error(request, 'Passwords do not match')
+    return render(request, 'register.html', locals())
+
+
+def logoutView(request):
+    logout(request)
+    return render('home')
+
